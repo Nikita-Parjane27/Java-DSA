e314ea7dd453f138618f32c461bc60c127148d06
Nikita Parjane <145220659+Nikita-Parjane27@users.noreply.github.com>
2026-01-12 16:36:48 +0000

    update

--- TopKelement.java
+++ TopKelement.java
@@ -1,25 +1,93 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class TopKelement {
 
-    public static void main(String[] args) {
+    //Top K Largest Elements
+
+   /* public static void main(String[] args) {
 
         int[] arr = {5, 12, 3, 17, 9, 21, 8};
         int k = 3;
 
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for (int num : arr) {
-            minHeap.add(num);
+            pq.add(num);
 
-            if (minHeap.size() > k) {
-                minHeap.poll(); // remove smallest
+            if (pq.size() < k) {
+                pq.poll(); // remove smallest element
             }
         }
 
-        System.out.println("Top " + k + " largest elements:");
-        while (!minHeap.isEmpty()) {
-            System.out.println(minHeap.poll());
+        System.out.println("Top " + k + " smallest elements:");
+        while (!pq.isEmpty()) {
+            System.out.println(pq.poll());
         }
     }
+
+    public static int[] topKElements(int[] arr, int k) {
+        if (k<=0) {
+            return new int[0];
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int id=0; id<arr.length; id++) {
+            int x=arr[id];
+            if (pq.size()<k) {
+                pq.offer(x);
+            }
+            else if(!pq.isEmpty() && x<pq.peek()) {
+                pq.poll();
+                pq.offer(x);
+            }
+        }
+        Integer[] result = pq.toArray(new Integer[0]);
+        int[] res = new int[result.length];
+        for(int id=0; id<result.length; id++) {
+            res[id]=result[id];
+        }
+        Arrays.sort(res);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 12, 3, 17, 9, 21, 8};
+        int k = 3;
+        int[] topK = topKElements(arr, k);
+        System.out.println("Top " + k + " smallest elements: " + Arrays.toString(topK));
+    }*/
+
+    //Top K Smallest Elements
+    /* public static int[] topKElements(int[] arr, int k) {
+        if (k<=0) {
+            return new int[0];
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int id=0; id<arr.length; id++) {
+            int x=arr[id];
+            if (pq.size()<k) {
+                pq.offer(x);
+            }
+            else if(!pq.isEmpty() && x<pq.peek()) {
+                pq.poll();
+                pq.offer(x);
+            }
+        }
+        Integer[] result = pq.toArray(new Integer[0]);
+        int[] res = new int[result.length];
+        for(int id=0; id<result.length; id++) {
+            res[id]=result[id];
+        }
+        Arrays.sort(res);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 12, 3, 17, 9, 21, 8};
+        int k = 3;
+        int[] topK = topKElements(arr, k);
+        System.out.println("Top " + k + " smallest elements: " + Arrays.toString(topK));
+    }*/
 }
